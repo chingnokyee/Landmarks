270b42f40b39558c0aed939c8159c2dbb2630bb2
Zym <chingnokyee@gmail.com>
2021-01-18 22:58:59 +0800

    Done Session 2

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,10 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, Zym!")
-            .padding()
+        Text("Turtle Rock")
             .font(.title)
-            .foregroundColor(.gray)
+            
     }
 }
 
